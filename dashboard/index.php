c81c1f2735bd995cb5f63887135ab3a91336068d
rafiradinka <rafi.radinka@gmail.com>
2024-12-01 22:29:13 +0700

    index dashboard pertama

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,1 +1,12 @@
-<a href="../auth/logout.php">logout</a>
+<?php include("../_header.php");?>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1>Dashboard</h1>
+            <p>Selamat datang <mark><?=$_SESSION['user']; ?></mark></p>
+            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+        </div>
+    </div>
+
+
+<?php include("../_footer.php");?>
